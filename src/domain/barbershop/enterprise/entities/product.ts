78508a905856a49731c34421c42303d50b5d50d8
doguebart <douglasweelber.2004@outlook.com>
2024-09-04 20:15:21 -0300

    feat: Create products entity

---
+++ src/domain/barbershop/enterprise/entities/product.ts
@@ -0,0 +1,57 @@
+import { Entity } from "src/core/entities/entity";
+import { UniqueEntityId } from "src/core/entities/unique-entity-id";
+
+export interface ProductProps {
+  name: string;
+  price: string;
+  quantity: number;
+  updatedAt?: Date | null;
+}
+
+export class Product extends Entity<ProductProps> {
+  get name() {
+    return this.props.name;
+  }
+
+  set name(name: string) {
+    this.props.name = name;
+    this.touch;
+  }
+
+  get price() {
+    return this.props.price;
+  }
+
+  set price(price: string) {
+    this.props.price = price;
+    this.touch;
+  }
+
+  get quantity() {
+    return this.props.quantity;
+  }
+
+  set quantity(quantity: number) {
+    this.props.quantity = quantity;
+    this.touch;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  static create(props: ProductProps, id?: UniqueEntityId) {
+    const product = new Product(
+      {
+        ...props,
+      },
+      id
+    );
+
+    return product;
+  }
+}
