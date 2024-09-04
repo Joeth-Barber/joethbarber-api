63a2d221a4e6bd94fe71f63059143c5fec05f273
doguebart <douglasweelber.2004@outlook.com>
2024-09-04 20:15:28 -0300

    feat: Create services entity

---
+++ src/domain/barbershop/enterprise/entities/service.ts
@@ -0,0 +1,36 @@
+import { Entity } from "src/core/entities/entity";
+import { UniqueEntityId } from "src/core/entities/unique-entity-id";
+
+export interface ServiceProps {
+  name: string;
+  price: string;
+}
+
+export class Service extends Entity<ServiceProps> {
+  get name() {
+    return this.props.name;
+  }
+
+  set name(name: string) {
+    this.props.name = name;
+  }
+
+  get price() {
+    return this.props.price;
+  }
+
+  set price(price: string) {
+    this.props.price = price;
+  }
+
+  static create(props: ServiceProps, id?: UniqueEntityId) {
+    const service = new Service(
+      {
+        ...props,
+      },
+      id
+    );
+
+    return service;
+  }
+}
