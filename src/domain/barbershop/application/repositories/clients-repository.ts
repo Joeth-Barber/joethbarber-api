7e67e9d6ab045afea702d0107fcaf1f320b53205
doguebart <douglasweelber.2004@outlook.com>
2024-09-05 15:44:57 -0300

    feat: Create new methods in clients repository

--- src/domain/barbershop/application/repositories/clients-repository.ts
+++ src/domain/barbershop/application/repositories/clients-repository.ts
@@ -1,6 +1,8 @@
 import { Client } from "../../enterprise/entities/client";
 
 export abstract class ClientsRepository {
+  abstract findByPhone(phone: string): Promise<Client | null>;
+  abstract findByCpf(cpf: string): Promise<Client | null>;
   abstract findByEmail(email: string): Promise<Client | null>;
   abstract create(client: Client): Promise<void>;
 }
--- src/domain/barbershop/enterprise/entities/value-objects/cpf.spec.ts
+++ src/domain/barbershop/enterprise/entities/value-objects/cpf.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from "vitest";
 import { CPF } from "./cpf";
 
 test("it should be able to check a cpf", () => {
-  const cpf = CPF.create("401.238.038-96");
+  const cpf = CPF.create("111.111.111-11");
 
-  expect(cpf.value).toEqual("40123803896");
+  expect(cpf.value).toEqual("11111111111");
 });
