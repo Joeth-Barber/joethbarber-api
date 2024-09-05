ad6042e4379b9b9567776e2de5855eb76297a765
doguebart <douglasweelber.2004@outlook.com>
2024-09-05 16:12:22 -0300

    feat: Add create barber use case

---
+++ src/domain/barbershop/application/use-cases/create-barber.ts
@@ -0,0 +1,51 @@
+import { Either, left, right } from "@/core/either";
+import { EmailAlreadyExistsError } from "@/core/errors/email-already-exists";
+import { Injectable } from "@nestjs/common";
+import { Barber } from "../../enterprise/entities/barber";
+import { HashGenerator } from "../cryptography/hash-generator";
+import { BarbersRepository } from "../repositories/barbers-repository";
+
+export interface CreateBarberUseCaseRequest {
+  fullName: string;
+  email: string;
+  password: string;
+}
+
+type CreateBarberUseCaseResponse = Either<
+  EmailAlreadyExistsError,
+  { barber: Barber }
+>;
+
+@Injectable()
+export class CreateBarberUseCase {
+  constructor(
+    private barbersRepository: BarbersRepository,
+    private hashGenerator: HashGenerator
+  ) {}
+
+  async execute({
+    fullName,
+    email,
+    password,
+  }: CreateBarberUseCaseRequest): Promise<CreateBarberUseCaseResponse> {
+    const emailAlreadyExists = await this.barbersRepository.findByEmail(email);
+
+    if (emailAlreadyExists) {
+      return left(new EmailAlreadyExistsError());
+    }
+
+    const password_hash = await this.hashGenerator.hash(password);
+
+    const barber = Barber.create({
+      fullName,
+      email,
+      password: password_hash,
+    });
+
+    await this.barbersRepository.create(barber);
+
+    return right({
+      barber,
+    });
+  }
+}
