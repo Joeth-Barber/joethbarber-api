e3748b8095192fffdaae494336b0f09cb7f5cce2
doguebart <douglasweelber.2004@outlook.com>
2024-09-06 16:50:40 -0300

    feat: Add create service use case

---
+++ src/domain/barbershop/application/use-cases/services/create-service.ts
@@ -0,0 +1,36 @@
+import { Either, right } from "@/core/either";
+import { EmailAlreadyExistsError } from "@/core/errors/email-already-exists";
+import { Injectable } from "@nestjs/common";
+import { Service } from "../../../enterprise/entities/service";
+import { ServicesRepository } from "../../repositories/services-repository";
+
+export interface CreateServiceUseCaseRequest {
+  name: string;
+  price: string;
+}
+
+type CreateServiceUseCaseResponse = Either<
+  EmailAlreadyExistsError,
+  { service: Service }
+>;
+
+@Injectable()
+export class CreateServiceUseCase {
+  constructor(private servicesRepository: ServicesRepository) {}
+
+  async execute({
+    name,
+    price,
+  }: CreateServiceUseCaseRequest): Promise<CreateServiceUseCaseResponse> {
+    const service = Service.create({
+      name,
+      price,
+    });
+
+    await this.servicesRepository.create(service);
+
+    return right({
+      service,
+    });
+  }
+}
