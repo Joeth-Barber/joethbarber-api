e867a4b3443001b6c41edd3b120f54416f734009
doguebart <douglasweelber.2004@outlook.com>
2024-09-06 16:09:00 -0300

    feat: Create find by id & save methods in barbers repository

--- test/repositories/in-memory-barbers-repository.ts
+++ test/repositories/in-memory-barbers-repository.ts
@@ -4,6 +4,22 @@ import { Barber } from "@/domain/barbershop/enterprise/entities/barber";
 export class InMemoryBarbersRepository implements BarbersRepository {
   public items: Barber[] = [];
 
+  async save(barber: Barber) {
+    const itemIndex = this.items.findIndex((item) => item.id === barber.id);
+
+    this.items[itemIndex] = barber;
+  }
+
+  async findById(id: string) {
+    const barber = this.items.find((item) => item.id.toString() === id);
+
+    if (!barber) {
+      return null;
+    }
+
+    return barber;
+  }
+
   async findByEmail(email: string) {
     const barber = this.items.find((item) => item.email === email);
 
