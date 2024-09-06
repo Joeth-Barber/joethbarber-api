d822fc982a1189ab59737060e6813dc23e51a90a
doguebart <douglasweelber.2004@outlook.com>
2024-09-06 18:07:28 -0300

    feat: Add create product use case

---
+++ src/domain/barbershop/application/use-cases/products/create-product.ts
@@ -0,0 +1,35 @@
+import { Either, right } from "@/core/either";
+import { Injectable } from "@nestjs/common";
+import { Product } from "../../../enterprise/entities/product";
+import { ProductsRepository } from "../../repositories/products-repository";
+
+export interface CreateProductUseCaseRequest {
+  name: string;
+  price: string;
+  quantity: number;
+}
+
+type CreateProductUseCaseResponse = Either<null, { product: Product }>;
+
+@Injectable()
+export class CreateProductUseCase {
+  constructor(private productsRepository: ProductsRepository) {}
+
+  async execute({
+    name,
+    price,
+    quantity,
+  }: CreateProductUseCaseRequest): Promise<CreateProductUseCaseResponse> {
+    const product = Product.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await this.productsRepository.create(product);
+
+    return right({
+      product,
+    });
+  }
+}
