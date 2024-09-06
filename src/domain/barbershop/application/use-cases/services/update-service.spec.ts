303e3fff8bd2432c31e9ccacbe4cd06f28e28ee2
doguebart <douglasweelber.2004@outlook.com>
2024-09-06 17:29:48 -0300

    test: Create update service use case unit test

---
+++ src/domain/barbershop/application/use-cases/services/update-service.spec.ts
@@ -0,0 +1,41 @@
+import { InMemoryServicesRepository } from "test/repositories/in-memory-services-repository";
+import { makeService } from "test/factories/make-service";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { UpdateServiceUseCase } from "./update-service";
+
+let inMemoryServicesRepository: InMemoryServicesRepository;
+let sut: UpdateServiceUseCase;
+
+describe("Update Service", () => {
+  beforeEach(() => {
+    inMemoryServicesRepository = new InMemoryServicesRepository();
+    sut = new UpdateServiceUseCase(inMemoryServicesRepository);
+  });
+
+  it("should be able to update a service", async () => {
+    await inMemoryServicesRepository.create(
+      makeService(
+        {
+          name: "Corte de cabelo",
+          price: "R$ 35,00",
+        },
+        new UniqueEntityId("service-01")
+      )
+    );
+    expect(inMemoryServicesRepository.items).toHaveLength(1);
+
+    const result = await sut.execute({
+      serviceId: new UniqueEntityId("service-01"),
+      name: "Corte de cabelo",
+      price: "R$ 25,00",
+    });
+
+    expect(result.isRight()).toBe(true);
+    expect(result.value).toEqual({
+      service: expect.objectContaining({
+        name: "Corte de cabelo",
+        price: "R$ 25,00",
+      }),
+    });
+  });
+});
