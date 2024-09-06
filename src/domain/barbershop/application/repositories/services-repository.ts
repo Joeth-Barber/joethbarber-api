b012d146b297925ce8e422356d7ea192f05fde0c
doguebart <douglasweelber.2004@outlook.com>
2024-09-06 17:36:30 -0300

    feat: Create find many method in services repository

--- src/domain/barbershop/application/repositories/services-repository.ts
+++ src/domain/barbershop/application/repositories/services-repository.ts
@@ -1,8 +1,10 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { Service } from "../../enterprise/entities/service";
 
 export abstract class ServicesRepository {
   abstract delete(service: Service): Promise<void>;
   abstract save(service: Service): Promise<void>;
   abstract findById(id: string): Promise<Service | null>;
+  abstract findMany(params: PaginationParams): Promise<Service[]>;
   abstract create(service: Service): Promise<void>;
 }
--- test/repositories/in-memory-services-repository.ts
+++ test/repositories/in-memory-services-repository.ts
@@ -1,8 +1,15 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { ServicesRepository } from "@/domain/barbershop/application/repositories/services-repository";
 import { Service } from "@/domain/barbershop/enterprise/entities/service";
 
 export class InMemoryServicesRepository implements ServicesRepository {
   public items: Service[] = [];
+
+  async findMany({ page }: PaginationParams) {
+    const services = this.items.slice((page - 1) * 10, page * 10);
+
+    return services;
+  }
 
   async delete(service: Service) {
     const itemIndex = this.items.findIndex((item) => item.id === service.id);
