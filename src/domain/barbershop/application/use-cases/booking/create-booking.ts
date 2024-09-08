78faf7b1916e09e55aa683600c6071d369beba12
doguebart <douglasweelber.2004@outlook.com>
2024-09-08 19:34:36 -0300

    feat: Add create booking use case

---
+++ src/domain/barbershop/application/use-cases/booking/create-booking.ts
@@ -0,0 +1,71 @@
+import { Injectable } from "@nestjs/common";
+import { Either, left, right } from "@/core/either";
+import { Booking } from "../../../enterprise/entities/booking";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { ResourceNotFoundError } from "@/core/errors/resource-not-found";
+import { Product } from "@/domain/barbershop/enterprise/entities/product";
+import { Service } from "@/domain/barbershop/enterprise/entities/service";
+import { BookingsRepository } from "../../repositories/bookings-repository";
+import { ClientsRepository } from "../../repositories/clients-repository";
+import { BookingDateOverlappingError } from "@/core/errors/booking-date-overlapping";
+
+interface CreateBookingUseCaseRequest {
+  clientId: UniqueEntityId;
+  date: Date;
+  totalPrice: number;
+  description: string;
+  products: Product[];
+  services: Service[];
+}
+
+type CreateBookingUseCaseResponse = Either<
+  ResourceNotFoundError,
+  { booking: Booking }
+>;
+
+@Injectable()
+export class CreateBookingUseCase {
+  constructor(
+    private bookingsRepository: BookingsRepository,
+    private clientsRepository: ClientsRepository
+  ) {}
+
+  async execute({
+    clientId,
+    date,
+    description,
+    products,
+    services,
+  }: CreateBookingUseCaseRequest): Promise<CreateBookingUseCaseResponse> {
+    const client = await this.clientsRepository.findById(clientId.toString());
+
+    if (!client) {
+      return left(new ResourceNotFoundError());
+    }
+
+    const isBookingOverlapping = await this.bookingsRepository.findByDate(date);
+
+    if (isBookingOverlapping) {
+      return left(new BookingDateOverlappingError());
+    }
+
+    const calculatedTotalPrice =
+      services.reduce((sum, service) => sum + service.price, 0) +
+      products.reduce((sum, product) => sum + product.price, 0);
+
+    const booking = Booking.create({
+      clientId,
+      date,
+      totalPrice: calculatedTotalPrice,
+      description,
+      products,
+      services,
+    });
+
+    await this.bookingsRepository.create(booking);
+
+    // TODO: o totalPrice dessa booking deve ser adicionado ao campo AMOUNT da entity Payment
+
+    return right({ booking });
+  }
+}
