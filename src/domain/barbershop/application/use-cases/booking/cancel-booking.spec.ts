8cebeb17e5e09f2f5c739f513366eb97e2caf8be
doguebart <douglasweelber.2004@outlook.com>
2024-09-08 21:36:59 -0300

    test: Create cancel booking use case unit test

---
+++ src/domain/barbershop/application/use-cases/booking/cancel-booking.spec.ts
@@ -0,0 +1,64 @@
+import { InMemoryBookingsRepository } from "test/repositories/in-memory-bookings-repository";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { makeBooking } from "test/factories/make-booking";
+import { CancelBookingUseCase } from "./cancel-booking";
+
+let inMemoryBookingsRepository: InMemoryBookingsRepository;
+let sut: CancelBookingUseCase;
+
+describe("Cancel Booking", () => {
+  beforeEach(() => {
+    inMemoryBookingsRepository = new InMemoryBookingsRepository();
+    sut = new CancelBookingUseCase(inMemoryBookingsRepository);
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  it("should be able to cancel a booking", async () => {
+    const date = new Date(2024, 8, 10, 14);
+    vi.setSystemTime(date);
+
+    const booking = makeBooking(
+      {
+        clientId: new UniqueEntityId("client-01"),
+        date: new Date("2024-09-10T17:00:00"),
+      },
+      new UniqueEntityId("booking-01")
+    );
+    inMemoryBookingsRepository.create(booking);
+    expect(inMemoryBookingsRepository.items).toHaveLength(1);
+
+    const result = await sut.execute({
+      bookingId: new UniqueEntityId("booking-01"),
+      clientId: new UniqueEntityId("client-01"),
+    });
+
+    expect(result.isRight()).toBe(true);
+    expect(inMemoryBookingsRepository.items[0].status).toBe("CANCELED");
+  });
+
+  it("should not be able to cancel a booking after the 3 hours limit", async () => {
+    const date = new Date(2024, 8, 10, 14);
+    vi.setSystemTime(date);
+
+    const booking = makeBooking(
+      {
+        clientId: new UniqueEntityId("client-01"),
+        date: new Date("2024-09-10T15:00:00"),
+      },
+      new UniqueEntityId("booking-01")
+    );
+    inMemoryBookingsRepository.create(booking);
+    expect(inMemoryBookingsRepository.items).toHaveLength(1);
+
+    const result = await sut.execute({
+      bookingId: new UniqueEntityId("booking-01"),
+      clientId: new UniqueEntityId("client-01"),
+    });
+
+    expect(result.isLeft()).toBe(true);
+  });
+});
