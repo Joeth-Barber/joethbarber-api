bde9771a55b816fd8c18419a22f13521a332f38f
doguebart <douglasweelber.2004@outlook.com>
2024-09-12 21:39:26 -0300

    feat: Create fetch payments by client id method in payments repository

--- src/domain/barbershop/application/repositories/payments-repository.ts
+++ src/domain/barbershop/application/repositories/payments-repository.ts
@@ -1,7 +1,12 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { Payment } from "../../enterprise/entities/payment";
 
 export abstract class PaymentsRepository {
   abstract save(payment: Payment): Promise<void>;
   abstract findById(id: string): Promise<Payment | null>;
+  abstract fetchByClientId(
+    params: PaginationParams,
+    id: string
+  ): Promise<Payment[]>;
   abstract create(payment: Payment): Promise<void>;
 }
--- test/repositories/in-memory-payments-repository.ts
+++ test/repositories/in-memory-payments-repository.ts
@@ -4,6 +4,23 @@ import { Payment } from "@/domain/barbershop/enterprise/entities/payment";
 export class InMemoryPaymentsRepository implements PaymentsRepository {
   public items: Payment[] = [];
 
+  async fetchByClientId(
+    { page }: { page: number },
+    id: string
+  ): Promise<Payment[]> {
+    const clientPayments = this.items
+      .filter((payment) => payment.clientId.toString() === id)
+      .sort((a, b) => {
+        if (!a.paymentDate) return 1;
+        if (!b.paymentDate) return -1;
+        return b.paymentDate.getTime() - a.paymentDate.getTime();
+      });
+
+    const payments = clientPayments.slice((page - 1) * 10, page * 10);
+
+    return payments;
+  }
+
   async save(payment: Payment) {
     const itemIndex = this.items.findIndex((item) => item.id === payment.id);
 
